dae38d19e034934f0d1b9a7abfc04f469bc4e9fa
Glitch (wnoizumi-fcc) <none>
2020-07-16 02:08:52 +0000

    Finished Basic Nodejs

--- myApp.js
+++ myApp.js
@@ -1,47 +1,62 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-// --> 7)  Mount the Logger middleware here
+
+//Logging middlewware
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 // --> 11)  Mount the body-parser middleware  here
 
-
-/** 3) Serve an HTML file */
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 
-/** 4) Serve static assets  */
+
 app.use(express.static(__dirname + "/public"));
 
-/** 5) serve JSON on a specific route */
 app.get("/json", (req, res) => {
   let message = process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON" : "Hello json";
   res.json({"message": message});
 });
 
-/** 6) Use the .env file to configure the app */
- 
- 
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
 
-
-/** 8) Chaining middleware. A Time server */
-
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+    res.json({"time" : req.time});
+});
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", (req, res) => {
+  let word = req.params.word;
+  res.json({"echo": word});
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route("/name")
+  .get((req, res) => {
+  var first = req.query.first;
+  var last = req.query.last;
+  res.json({"name": first + " " + last});
+}).post((req, res) => {
+  let body = req.body;
+  res.json({"name": body.first + " " + body.last});
+});
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
+
 
 
 /** 12) Get data form POST  */
